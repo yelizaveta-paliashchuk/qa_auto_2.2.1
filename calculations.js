f751f0ee71cfe81c805b8fc443aedc20ff503d4a
LisaPaliashchuk <yelizaveta.paliashchuk@Orchestr8.com>
2024-03-01 13:10:31 +0100

    bug fixed in calculations.js (HW2.2.1.)

--- calculations.js
+++ calculations.js
@@ -1,25 +1,24 @@
 function increaseBalance(x, y) {
-  x = x + y;
-  return x;
+  x = x + y
+  return x
 }
 
-function decreaseBalance(x) {
-x = x - y;
-return x;
+function decreaseBalance(x, y) {
+  x = x - y
+  return x
 }
 
 function divideBalanceByAccounts(x, y) {
-  x = x / y;
-  return x;
-  console.log("x");
+  x = x / y
+  return x
 }
 
 function getRestAfterDivision(x, y) {
-  x = x % y;
-  return x;
+  x = x % y
+  return x
 }
 
-console.log(increaseBalance(3000, 700));
-console.log(decreaseBalance(3000, 700));
-console.log(divideBalanceByAccounts(3000, 2, 4000));
-console.log(getRestAfterDivision(7000, 3));
+console.log(increaseBalance(3000, 700))
+console.log(decreaseBalance(3000, 700))
+console.log(divideBalanceByAccounts(3000, 2))
+console.log(getRestAfterDivision(7000, 3))
